35db95f765d4beb1cced67e05fb7461925c80458
maidaniuk-dev <vla.maidaniuk@gmail.com>
2025-07-11 00:57:53 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello from B2B SaaS API"}
+
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
